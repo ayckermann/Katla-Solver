49648a876699090b1016af13406fc9124ec4c6dc
Ayatullah Ma'arif <88651324+ayckermann@users.noreply.github.com>
2022-04-12 11:42:26 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Katla_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
